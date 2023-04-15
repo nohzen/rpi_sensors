6beecf44793f8463370d56053c7cc830e95a5013
nohzen <29157627+nohzen@users.noreply.github.com>
2023-04-15 22:07:04 +0900

    feat: Use argparse & Fix module names

--- main_sensor.py
+++ main_sensor.py
@@ -1,22 +1,16 @@
-import time, datetime, csv
+import os
+import time, datetime
+import csv
+from co2_mh_z19 import Co2SensorMhZ19
+from thermometer_adt7410 import adt7410_read
+from thermometer_sht31 import HumidityTemperatureSensorSht31
 
 
-mode = "office" # "office"
-
-if mode == "home":
-    csv_path = "data_home.csv"
-
-    from mod_co2_mh_z19 import Co2SensorMhZ19
-    from mod_thermometer_adt7410 import adt7410_read
-elif mode == "office":
-    csv_path = "data_office.csv"
-
-    from mod_co2_mh_z19 import Co2SensorMhZ19
-    from mod_thermometer_sht31 import HumidityTemperatureSensorSht31
+sht31_sensor = HumidityTemperatureSensorSht31()
+co2_sensor = Co2SensorMhZ19(serial_device="/dev/ttyS0", abc=False)
 
 
-# Write header
-if False:
+def write_header(csv_path, place):
     with open(csv_path, 'a') as file:
         csv_writer = csv.writer(file)
         # csv_writer.writerow(["datetime",
@@ -24,40 +18,54 @@ if False:
         #                      "adt7410_temperature",
         #                      "mhz19_1_co2", "mhz19_1_temperature",
         #                      "mhz19_2_co2", "mhz19_2_temperature"])
-        if mode == "home":
+        if place == "home":
             csv_writer.writerow(["datetime",
                                 "adt7410_temperature",
                                 "mhz19_co2", "mhz19_temperature"])
-        elif mode == "office":
+        elif place == "office":
             csv_writer.writerow(["datetime",
                                 "sht31_temperature", "sht31_humidity",
                                 "mhz19_co2", "mhz19_temperature"])
         else:
-            raise ValueError()
+            raise NotImplementedError()
 
 
-sht31_sensor = HumidityTemperatureSensorSht31()
-co2_sensor = Co2SensorMhZ19(serial_device="/dev/ttyS0", abc=False)
-# co2_sensor = Co2SensorMhZ19(serial_device="/dev/ttyAMA1", abc=False)
-
-def main():
+def read_sensor(csv_path, place):
     now = datetime.datetime.now()
 
-    if mode == "home":
+    if place == "home":
         adt7410_temperature = adt7410_read()
         mhz19_co2, mhz19_temperature = co2_sensor.readall()
         with open(csv_path, 'a') as file:
             csv_writer = csv.writer(file)
             csv_writer.writerow([now, adt7410_temperature, mhz19_co2, mhz19_temperature])
-    elif mode == "office":
+    elif place == "office":
         sht31_temperature, sht31_humidity = sht31_sensor.read()
         mhz19_co2, mhz19_temperature = co2_sensor.readall()
         # print(mhz19_co2, sht31_temperature, sht31_humidity)
         with open(csv_path, 'a') as file:
             csv_writer = csv.writer(file)
             csv_writer.writerow([now, sht31_temperature, sht31_humidity, mhz19_co2, mhz19_temperature])
+    else:
+        raise NotImplementedError()
 
 
-while True:
-    main()
-    time.sleep(60.0)
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--csv_path", default="./datas/data.csv",
+                        help="Path to csv file")
+    parser.add_argument("--place", default="office", choices=["office", "home"],
+                        help="place to use (determine sensors)")
+    args = parser.parse_args()
+
+    csv_path = args.csv_path
+    place = args.place
+
+    if not os.path.isfile(csv_path):
+        write_header(csv_path, place)
+
+    while True:
+        read_sensor(csv_path, place)
+        time.sleep(60.0)
